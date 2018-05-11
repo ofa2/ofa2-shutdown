3335326fac3dab26c53a0c6b345e82a8f2cf6b12
xinshangshangxin <xinshangshangxin@gmail.com>
2018-05-11 16:38:28 +0800

    feat: init

---
+++ dist/bundle.cjs.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function invokeCallback(done) {
+  process.nextTick(done);
+}
+
+function lift() {
+  let beforeShutdown = this.config.beforeShutdown || invokeCallback;
+  process.on('SIGINT', async () => {
+    this.logger.info('Shutting down...');
+    let startTime = Date.now();
+    let err;
+
+    try {
+      await beforeShutdown();
+      this.logger.info(`Shuted down...(in ${Date.now() - startTime}ms)`);
+    } catch (e) {
+      err = e;
+    }
+
+    this.lower().on('lowered', () => {
+      process.exit(err ? 1 : 0);
+    });
+  });
+  return new Promise(resolve => {
+    resolve();
+  });
+}
+
+module.exports = lift;
+//# sourceMappingURL=bundle.cjs.js.map
